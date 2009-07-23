00bccfdd80fc581025084747251b284fac1eac18
Shinsuke Matsui <poppen.jp@gmail.com>
2009-07-23 17:51:05 +0900

    removed checking of User-Agent.

--- application/controller/proxy.py
+++ application/controller/proxy.py
@@ -5,18 +5,12 @@ import os, yaml
 from libs.amazon import ProductAdvertising
 
 class ProxyController(BaseController):
-    def _is_ua_proxy(self):
-        if 'rpaproxy' in self.request.headers['User-Agent']:
-            return True
-        else:
-            return False
-
     def proxy(self):
         xml_file = os.path.join( os.path.dirname(__file__), '../../amazon-auth-proxy.yaml' )
         conf = yaml.load( open(xml_file) )
         pa = ProductAdvertising(conf, self.params)
 
-        if conf['use_redirect'] and self._is_ua_proxy():
+        if conf['use_redirect']:
             self.redirect( pa.build_url() )
         else:
             self.render(
